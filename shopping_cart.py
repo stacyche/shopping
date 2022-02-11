73b7ae6fec0b5b1845c362ab0ed3fe4f7a7b6e84
stacyche <xc172@georgetown.edu>
2022-02-11 18:39:38 -0500

    total price

--- shopping_cart.py
+++ shopping_cart.py
@@ -47,6 +47,9 @@ def to_usd(my_price):
 
 
 # ASK FOR USER INPUT 
+
+total_price = 0 
+
 while True:
     selected_id = input("Please input a product identifier") # > string 
     if selected_id == "DONE" or selected_id == "done" or selected_id == "Done": 
@@ -54,10 +57,14 @@ while True:
     else: 
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
         print("SELECTED PRODCUT: " + matching_product["name"] + " " + str(matching_product["price"])) #>"9"
 
 
-    
+# INFO DISPLY / OUTPUT 
+
+
+print("TOTAL PRICE: " + str(total_price))  # format as USD 
 
 
 
