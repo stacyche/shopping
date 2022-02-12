9f1de07a34e9c5a5e1e38c99800e6245d5230c91
stacyche <xc172@georgetown.edu>
2022-02-11 19:01:09 -0500

    adding all the selected products

--- shopping_cart.py
+++ shopping_cart.py
@@ -49,19 +49,29 @@ def to_usd(my_price):
 # ASK FOR USER INPUT 
 
 total_price = 0 
+selected_ids = []
 
 while True:
     selected_id = input("Please input a product identifier") # > string 
     if selected_id == "DONE" or selected_id == "done" or selected_id == "Done": 
         break 
     else: 
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODCUT: " + matching_product["name"] + " " + str(matching_product["price"])) #>"9"
-
+       # matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        # matching_product = matching_products[0]
+        # total_price = total_price + matching_product["price"]
+        # print("SELECTED PRODCUT: " + matching_product["name"] + " " + str(matching_product["price"])) #>"9"
+        selected_ids.append(selected_id)
 
 # INFO DISPLY / OUTPUT 
+
+#print(selected_ids)
+
+for selected_id in selected_ids: 
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("SELECTED PRODCUT: " + matching_product["name"] + " " + str(matching_product["price"])) #>"9"
+
 
 
 print("TOTAL PRICE: " + str(total_price))  # format as USD 
